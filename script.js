53b0fdd084eedf60a750e7a637dd6f9beacf6e4f
Mr Standly <standlyfaustina@gmail.com>
2022-01-03 18:25:03 +0100

    typing effect w js

---
+++ script.js
@@ -0,0 +1,15 @@
+const heading ="Please Meet our Team";
+let i =0;
+const typing = () =>{
+    if( i < heading.length){
+        document.querySelector(
+            '.heading'
+        ).innerHTML += heading.charAt(i);
+        i++;
+        setTimeout(typing, 150);
+    }
+
+
+}
+
+typing();
